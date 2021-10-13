b815fed041f29ed8bd8dc95fac7ca2a338c39682
carlolis <cilieff@gmail.com>
2021-10-13 21:31:32 +0200

    Widget et apelle à l'API https://adapools.org/

---
+++ assets/ts/controller.ts
@@ -0,0 +1,47 @@
+import { API_Url, TIMEOUT_SECONDS } from './config.js';
+
+
+
+
+const timeout = function (s: number): Promise<Response> {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+
+const AJAX = async  (url: string) => {
+ 
+    const fetchPro = fetch(url);
+
+    const res = await Promise.race([timeout(TIMEOUT_SECONDS), fetchPro]);
+
+    const data :PoolData = await res.json();
+    console.log(data)
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+    
+    
+
+    return data;
+};
+
+const widget = async () => {
+
+  const dataPool = await AJAX(API_Url);
+
+  document.querySelector(".pool-title")?.append(dataPool.data.db_name);
+  document.querySelector("[data-name=pool-updated]")?.append(dataPool.created);
+}
+
+
+widget()
+
+
+
+
+
+
