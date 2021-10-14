a78304dee7df7953914cb1ef8e77cb5fbf56a706
rmikael <63395897+rmikael@users.noreply.github.com>
2021-10-14 21:42:49 +0200

    style pour widget

--- assets/ts/controller.ts
+++ assets/ts/controller.ts
@@ -1,7 +1,4 @@
-import { API_Url, TIMEOUT_SECONDS } from './config.js';
-
-
-
+import { API_Url, TIMEOUT_SECONDS } from "./config.js";
 
 const timeout = function (s: number): Promise<Response> {
   return new Promise(function (_, reject) {
@@ -11,37 +8,24 @@ const timeout = function (s: number): Promise<Response> {
   });
 };
 
+const AJAX = async (url: string) => {
+  const fetchPro = fetch(url);
 
-const AJAX = async  (url: string) => {
- 
-    const fetchPro = fetch(url);
+  const res = await Promise.race([timeout(TIMEOUT_SECONDS), fetchPro]);
 
-    const res = await Promise.race([timeout(TIMEOUT_SECONDS), fetchPro]);
+  const data: PoolData = await res.json();
+  if (!res.ok) {
+    throw new Error(`${data.message} (${res.status})`);
+  }
 
-    const data :PoolData = await res.json();
-    console.log(data)
-    if (!res.ok) {
-      throw new Error(`${data.message} (${res.status})`);
-    }
-    
-    
-
-    return data;
+  return data;
 };
 
 const widget = async () => {
-
   const dataPool = await AJAX(API_Url);
 
   document.querySelector(".pool-title")?.append(dataPool.data.db_name);
   document.querySelector("[data-name=pool-updated]")?.append(dataPool.created);
-}
+};
 
-
-widget()
-
-
-
-
-
-
+widget();
