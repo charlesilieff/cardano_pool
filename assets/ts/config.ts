0947e975dc8dca451465943312999687fb6f7ea2
rmikael <adapool_mikael@protonmail.com>
2021-11-11 18:02:36 +0100

    nettoyage pour publication

--- assets/ts/config.ts
+++ assets/ts/config.ts
@@ -1,2 +1,2 @@
-export const API_Url = `https://js.adapools.org/pools/7f6c103302f96390d478a170fe80938b76fccd8a23490e3b6ddebcf7/summary.json`;
+export const API_Url = `https://js.adapools.org/pools/*********/summary.json`;
 export const TIMEOUT_SECONDS = 10;
