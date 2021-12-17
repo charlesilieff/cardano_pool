3e763f484ab76bc8f00fca1acd92f79252e1131e
carlolis <carlolis@MacBook-Pro-de-Charles.local>
2021-12-17 19:16:48 +0100

    reorganisation des fichiers

---
+++ assets/ts/content/french.ts
@@ -0,0 +1,25 @@
+import { Content } from '../models/content';
+
+export const frenchContent: Content = {
+  translation: {
+    whoAreWe: 'Qui sommes-nous ?',
+    whyJoinUs: 'Pourquoi s’engager avec nous ?',
+    poolDetails: 'Pool details', //FIXME
+    s1: {
+      p1: 'We are Charles 😊 and Mika 😉, a partnership of two French entrepreneurs motivated to democratize the use of crypto-currencies.',
+      p2: 'Mika, who helps administrations in their digital transformations, brings his technical and human experience. Charles brings over a decade of experience in software engineering. Together, we are enthusiastically participating in the development of the Cardano network!',
+      p3: 'We have been involved in cryptocurrencies since 2017, when we had the chance to make a profit at the top of the market. We discovered the Cardano project at that moment, and since then, we have been following it with great interest. Our goal with ADA Pool Europe is to create a community of people who share our values, believe in Cardano, and to which we would like to offer a good capital pooling experience.',
+      p4: 'Our goal with ADA Pool Europe is to create a community of people who share our values, believe in Cardano and to which we offer a good capital pooling experience.',
+    },
+    s2: {
+      p1: 'We are motivated to democratize the use of crypto-currencies.',
+      p2: ' high availability infrastructure',
+      p3: ' with the lowest possible fees',
+      p4: ' open to the largest audience possible',
+      p5: ' dedicated and secure servers',
+      p6: 'Our Europe-based Cardano staking pool is dedicated to strengthening and decentralizing the Cardano network. \n We offer high-availability, reliable and secure staking, based on two relay nodes and one producer node in enterprise-class server centers. Our goal is to create a good participant experience.',
+      p7: "We put a strong focus on the performance and security of our pool infrastructure. We would like to allow as many people as possible to participate in the crypto adventure. That's why, unlike many other pools, we don't charge variable fees (0%).",
+      p8: 'By staking your ADAs in our pool, you participate in the operation of the Cardano network.',
+    },
+  },
+};
---
+++ assets/ts/models/content.ts
@@ -0,0 +1,25 @@
+import { ResourceLanguage } from 'i18next';
+
+export interface Content extends ResourceLanguage {
+  translation: {
+    whoAreWe: string;
+    whyJoinUs: string;
+    poolDetails: string;
+    s1: {
+      p1: string;
+      p2: string;
+      p3: string;
+      p4: string;
+    };
+    s2: {
+      p1: string;
+      p2: string;
+      p3: string;
+      p4: string;
+      p5: string;
+      p6: string;
+      p7: string;
+      p8: string;
+    };
+  };
+}
